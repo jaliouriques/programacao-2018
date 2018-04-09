a3313d9e09b8b6877bd4849ff8037f0202be3c94
jaliouriques <jali.ouriques.90@gmail.com>
2018-04-09 13:41:29 -0300

    Add files via upload

---
+++ programacao/app/controlers/categorias.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_GET['acao'])){
+    $acao = $_GET['acao'];
+}else{
+    $acao = 'index';
+}
+
+if($acao=='index'){
+    $crud= new CategoriaCrud();
+    $categorias = $crud->getCategorias();
+
+    include '../views/listaCategorias.php';
+}
