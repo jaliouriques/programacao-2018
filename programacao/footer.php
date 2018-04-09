8e7f867f7af070d4ddb9ca95985c58207c8c11f0
jaliouriques <jali.ouriques.90@gmail.com>
2018-04-09 13:43:04 -0300

    Create footer.php

---
+++ programacao/footer.php
@@ -0,0 +1,3 @@
+<div style="text-align: center; bottom: 0; width: 110%; background-color: black; color: white; position: fixed; margin: auto; margin-left: -50px">
+    Cecília Borba e Jalieny Ouriques - 3InfoI1
+</div>
